913b70efbcfc7dc5acc639b0e16b368e58a9cce9
klocel-auto <null>
2024-01-16 13:36:47 +0000

    🔄 synced local 'web' with remote 'web'

--- web/pages/locations/index.tsx
+++ web/pages/locations/index.tsx
@@ -186,7 +186,6 @@ const LocationPages: PageComponent = () => {
                         showRangeLocationsModal,
                         setShowRangeLocationsModal
                     }}
-                    id={'a'}
                 />
             ) : (
                 <></>
