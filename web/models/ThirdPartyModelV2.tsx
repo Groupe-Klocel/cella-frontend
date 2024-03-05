946376b413bed375d56cf02b149673ab5c41a8a0
klocel-auto <null>
2024-03-05 13:33:23 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/models/ThirdPartyModelV2.tsx
+++ web/models/ThirdPartyModelV2.tsx
@@ -62,7 +62,7 @@ export const ThirdPartyModelV2: ModelType = {
             isDefaultHiddenList: false,
             isExcludedFromList: false,
             isSortable: true,
-            searchingFormat: null,
+            searchingFormat: 'String',
             isDetailRequested: true,
             isExcludedFromDetail: false,
             detailGroup: '{"label":"general", "position":0}',
