fe1b3ebd36b0d3b36d26771f76372c369064d8ea
klocel-auto <null>
2024-03-25 10:11:54 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/fake-data/stock.js
+++ web/fake-data/stock.js
@@ -1,16 +1,16 @@
 export const stockData = [
-	{
-		"id":1,
-		"name":"VENTES",
-		"value":0,
-		"system":"Yes",
-		"comment":"Etat vente",
-	},
-	{
-		"id":2,
-		"name":"QUALITE",
-		"value":10,
-		"system":"Yes",
-		"comment":"Etat qualité",
-	},
-]
+    {
+        id: 1,
+        name: 'VENTES',
+        value: 0,
+        system: 'Yes',
+        comment: 'Etat vente'
+    },
+    {
+        id: 2,
+        name: 'QUALITE',
+        value: 10,
+        system: 'Yes',
+        comment: 'Etat qualité'
+    }
+];
---
+++ web/pages/currencies/edit/[id].tsx
@@ -0,0 +1,69 @@
+/**
+CELLA Frontend
+Website and Mobile templates that can be used to communicate
+with CELLA WMS APIs.
+Copyright (C) 2023 KLOCEL <contact@klocel.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+**/
+import { AppHead, HeaderContent } from '@components';
+import { useRouter } from 'next/router';
+import { FC, useState } from 'react';
+import MainLayout from '../../../components/layouts/MainLayout';
+import { SingleParameterModelV2 as model } from 'models/SingleParameterModelV2';
+import useTranslation from 'next-translate/useTranslation';
+import { currenciesRoutes as itemRoutes } from 'modules/Currencies/Static/currenciesRoutes';
+import { META_DEFAULTS } from '@helpers';
+import { EditConfigParamComponent } from 'modules/Crud/EditConfigParamComponentV2';
+
+type PageComponent = FC & { layout: typeof MainLayout };
+
+const EditCurrencyPage: PageComponent = () => {
+    const { t } = useTranslation();
+
+    const router = useRouter();
+    const [data, setData] = useState<any>();
+    const { id } = router.query;
+
+    const breadsCrumb = [
+        ...itemRoutes,
+        {
+            breadcrumbName: `${data?.value}`
+        }
+    ];
+
+    return (
+        <>
+            <AppHead title={META_DEFAULTS.title} />
+            <EditConfigParamComponent
+                id={id!}
+                setData={setData}
+                dataModel={model}
+                headerComponent={
+                    <HeaderContent
+                        title={`${t('actions:edit-currency')} ${data?.value}`}
+                        routes={breadsCrumb}
+                        onBack={() => router.back()}
+                    />
+                }
+                comeFromFiltered={true}
+                routeAfterSuccess={`/currencies/:id`}
+            />
+        </>
+    );
+};
+
+EditCurrencyPage.layout = MainLayout;
+
+export default EditCurrencyPage;
