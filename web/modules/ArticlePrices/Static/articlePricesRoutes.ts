db173f8b019fad20f9fdcc9553fe73fa861e5897
klocel-auto <null>
2024-04-15 08:31:16 +0000

    🔄 synced local 'web/' with remote 'web/'

---
+++ web/modules/ArticlePrices/Static/articlePricesRoutes.ts
@@ -0,0 +1,36 @@
+/**
+CELLA Frontend
+Website and Mobile templates that can be used to communicate
+with CELLA WMS APIs.
+Copyright (C) 2023 KLOCEL <contact@klocel.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+**/
+// Breadcrumb Name should be the same as in the translation file
+export const articlePricesRoutes = [
+    {
+        breadcrumbName: 'menu:configuration'
+    },
+    {
+        breadcrumbName: 'menu:article-prices',
+        path: '/article-prices'
+    }
+];
+export const addArticlePriceRoutes = [
+    ...articlePricesRoutes,
+    {
+        path: '/add-article-price',
+        breadcrumbName: 'menu:add-article-price'
+    }
+];
