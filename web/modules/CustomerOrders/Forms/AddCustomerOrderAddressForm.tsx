ff947a76a6982e0d49c34a4c14cdfb9464c2d317
klocel-auto <null>
2024-06-05 09:23:48 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/modules/CustomerOrders/Forms/AddCustomerOrderAddressForm.tsx
+++ web/modules/CustomerOrders/Forms/AddCustomerOrderAddressForm.tsx
@@ -243,7 +243,7 @@ export const AddCustomerOrderAddressForm = (props: ISingleItemProps) => {
         filters: contactFilters,
         orderBy: null,
         page: 1,
-        itemsPerPage: 100
+        itemsPerPage: 100000
     });
 
     useEffect(() => {
