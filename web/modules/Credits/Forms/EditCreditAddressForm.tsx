b5af1f40d5aaeb0d17df43e0a4784549288f05e8
klocel-auto <null>
2024-06-05 12:35:03 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/modules/Credits/Forms/EditCreditAddressForm.tsx
+++ web/modules/Credits/Forms/EditCreditAddressForm.tsx
@@ -121,7 +121,7 @@ export const EditCreditAddressForm: FC<EditCreditAddressFormProps> = ({
         filters: contactFilters,
         orderBy: null,
         page: 1,
-        itemsPerPage: 100000
+        itemsPerPage: 100
     });
 
     useEffect(() => {
