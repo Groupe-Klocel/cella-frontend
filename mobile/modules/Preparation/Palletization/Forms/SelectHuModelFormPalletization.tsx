1dd6857adaa0656e74ce9cc4a26c2b8f035fbd96
klocel-auto <null>
2024-12-28 18:15:07 +0000

    🔄 synced local 'mobile/' with remote 'mobile/'

--- mobile/modules/Preparation/Palletization/Forms/SelectHuModelFormPalletization.tsx
+++ mobile/modules/Preparation/Palletization/Forms/SelectHuModelFormPalletization.tsx
@@ -3,68 +3,78 @@ CELLA Frontend
 Website and Mobile templates that can be used to communicate
 with CELLA WMS APIs.
 Copyright (C) 2023 KLOCEL <contact@klocel.com>
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 **/
 //DESCRIPTION: select manually or automatically one location in a list of locations according to their level
 
 import { WrapperForm, StyledForm, StyledFormItem, RadioButtons } from '@components';
-import { LsIsSecured, showError } from '@helpers';
+import { LsIsSecured, extractGivenConfigsParams, showError } from '@helpers';
 import { Form, Select } from 'antd';
 import { useAuth } from 'context/AuthContext';
-import { GetAllStockOwnersQuery, useGetAllStockOwnersQuery } from 'generated/graphql';
-import CameraScanner from 'modules/Common/CameraScanner';
 import useTranslation from 'next-translate/useTranslation';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import configs from '../../../../../common/configs.json';
+import parameters from '../../../../../common/parameters.json';
+import { GetHandlingUnitModelsQuery, useGetHandlingUnitModelsQuery } from 'generated/graphql';
+import CameraScanner from 'modules/Common/CameraScanner';
 
-export interface ISelectStockOwnerProps {
+export interface ISelectHuModelProps {
     process: string;
     stepNumber: number;
+    label: string;
     trigger: { [label: string]: any };
     buttons: { [label: string]: any };
     defaultValue?: any;
 }
 
-export const SelectStockOwnerForm = ({
+export const SelectHuModelFormPalletization = ({
     process,
     stepNumber,
-    defaultValue,
     trigger: { triggerRender, setTriggerRender },
-    buttons
-}: ISelectStockOwnerProps) => {
+    buttons,
+    defaultValue
+}: ISelectHuModelProps) => {
     const { graphqlRequestClient } = useAuth();
     const { t } = useTranslation();
     const storage = LsIsSecured();
     const storedObject = JSON.parse(storage.get(process) || '{}');
-
-    const [stockOwners, setStockOwners] = useState<Array<any>>();
-
+    const router = useRouter();
+    const { locale } = router;
     // TYPED SAFE ALL
+    const [huModels, setHuModels] = useState<Array<any>>();
     //camera scanner section
     const [form] = Form.useForm();
     const [camData, setCamData] = useState();
 
+    let huoId: any;
+    let huModelFind: any;
+    if (storedObject?.step10?.data?.handlingUnit?.handlingUnitOutbounds) {
+        huoId =
+            storedObject?.step10?.data?.handlingUnit?.handlingUnitOutbounds[0]?.handlingUnitModelId;
+
+        huModelFind = huModels?.find((element) => element.key === huoId);
+    }
+
     useEffect(() => {
         if (camData) {
-            if (stockOwners?.some((option) => option.text === camData)) {
-                const soToFind = stockOwners?.find((option) => option.text === camData);
-                form.setFieldsValue({ stockOwner: soToFind.key });
+            if (huModels?.some((option) => option.text.startsWith(camData))) {
+                const humToFind = huModels?.find((option) => option.text.startsWith(camData));
+                form.setFieldsValue({ huModel: humToFind.key });
             } else {
                 showError(t('messages:unexpected-scanned-item'));
             }
         }
-    }, [camData, stockOwners]);
+    }, [camData, huModels]);
 
     const handleCleanData = () => {
         form.resetFields();
@@ -72,44 +82,15 @@ export const SelectStockOwnerForm = ({
     };
     // end camera scanner section
 
-    //SelectStockOwner-1: retrieve stockOwners
-    const stockOwnersList = useGetAllStockOwnersQuery<Partial<GetAllStockOwnersQuery>, Error>(
-        graphqlRequestClient,
-        {
-            filters: {},
-            orderBy: null,
-            page: 1,
-            itemsPerPage: 100
-        }
-    );
-
-    useEffect(() => {
-        if (stockOwnersList) {
-            const newStockOwnerTexts: Array<any> = [];
-            const tmp = stockOwnersList?.data?.stockOwners?.results;
-            if (tmp) {
-                tmp.forEach((item) => {
-                    newStockOwnerTexts.push({ key: item.id, text: item.name });
-                });
-                setStockOwners(newStockOwnerTexts);
-            }
-        }
-    }, [stockOwnersList.data]);
-
     //Pre-requisite: initialize current step
     useEffect(() => {
-        //automatically set returnDate when defaultValue is provided
-        if (defaultValue) {
-            // N.B.: in this case previous step is kept at its previous value
+        if (huoId && huModels) {
             const data: { [label: string]: any } = {};
-            data['stockOwner'] = defaultValue;
+            data['handlingUnitModel'] = {
+                id: huModelFind.key,
+                name: huModelFind.text.split('-')[0]
+            };
             storedObject[`step${stepNumber}`] = { ...storedObject[`step${stepNumber}`], data };
-            setTriggerRender(!triggerRender);
-        } else if (stockOwnersList?.data?.stockOwners?.results.length === 1) {
-            const data: { [label: string]: any } = {};
-            data['stockOwner'] = stockOwnersList?.data?.stockOwners?.results[0];
-            storedObject[`step${stepNumber}`] = { ...storedObject[`step${stepNumber}`], data };
-            storage.set(process, JSON.stringify(storedObject));
             setTriggerRender(!triggerRender);
         } else if (storedObject.currentStep < stepNumber) {
             //check workflow direction and assign current step accordingly
@@ -117,26 +98,57 @@ export const SelectStockOwnerForm = ({
             storedObject.currentStep = stepNumber;
         }
         storage.set(process, JSON.stringify(storedObject));
-    }, [stockOwnersList.data]);
+    }, [huModels]);
 
-    //SelectStockOwner-2a: retrieve chosen stockOwner from select and set information
+    //SelectHuModel: retrieve huModel choices for select
+    const huModelList = useGetHandlingUnitModelsQuery<Partial<GetHandlingUnitModelsQuery>, Error>(
+        graphqlRequestClient,
+        {
+            filters: {
+                status: [configs.HANDLING_UNIT_MODEL_STATUS_IN_PROGRESS],
+                category: [
+                    parameters['HANDLING_UNIT_MODEL_CATEGORY_INBOUND/STOCK/OUTBOUND'],
+                    parameters['HANDLING_UNIT_MODEL_CATEGORY_OUTBOUND'],
+                    parameters['HANDLING_UNIT_MODEL_CATEGORY_INBOUND/OUTBOUND'],
+                    parameters['HANDLING_UNIT_MODEL_CATEGORY_OUTBOUND/STOCK']
+                ],
+                type: [parameters['HANDLING_UNIT_TYPE_PALLET']]
+            },
+            orderBy: null,
+            page: 1,
+            itemsPerPage: 100
+        }
+    );
+
+    useEffect(() => {
+        if (huModelList) {
+            const newTypeTexts: Array<any> = [];
+            const cData = huModelList?.data?.handlingUnitModels?.results;
+            if (cData) {
+                cData.forEach((item) => {
+                    newTypeTexts.push({ key: item.id, text: `${item.name} - ${item.description}` });
+                });
+                setHuModels(newTypeTexts);
+            }
+        }
+    }, [huModelList.data]);
+
+    //SelectHuModel-2a: retrieve chosen level from select and set information
     const onFinish = (values: any) => {
         const data: { [label: string]: any } = {};
-        const selectedStockOwner = stockOwnersList?.data?.stockOwners?.results.find((e: any) => {
-            return e.id == values.stockOwner;
+        const selectedHuModel = huModelList?.data?.handlingUnitModels?.results.find((e: any) => {
+            return e.id == values.huModel;
         });
-        data['stockOwner'] = selectedStockOwner;
+        data['handlingUnitModel'] = selectedHuModel;
         storedObject[`step${stepNumber}`] = { ...storedObject[`step${stepNumber}`], data };
         storage.set(process, JSON.stringify(storedObject));
         setTriggerRender(!triggerRender);
     };
 
-    //SelectStockOwner-2b: handle back to previous step settings
+    //SelectHuModel-2b: handle back to previous step settings
     const onBack = () => {
         setTriggerRender(!triggerRender);
-        for (let i = storedObject[`step${stepNumber}`].previousStep; i <= stepNumber; i++) {
-            delete storedObject[`step${i}`]?.data;
-        }
+        delete storedObject[`step${storedObject[`step${stepNumber}`].previousStep}`].data;
         storedObject.currentStep = storedObject[`step${stepNumber}`].previousStep;
         storage.set(process, JSON.stringify(storedObject));
     };
@@ -153,8 +165,8 @@ export const SelectStockOwnerForm = ({
                 form={form}
             >
                 <StyledFormItem
-                    label={t('common:stock-owner')}
-                    name="stockOwner"
+                    label={t('common:handling-unit-model')}
+                    name="huModel"
                     rules={[{ required: true, message: t('messages:error-message-empty-input') }]}
                 >
                     <Select
@@ -167,7 +179,7 @@ export const SelectStockOwnerForm = ({
                         }
                         allowClear
                     >
-                        {stockOwners?.map((option: any) => (
+                        {huModels?.map((option: any) => (
                             <Select.Option key={option.key} value={option.key}>
                                 {option.text}
                             </Select.Option>
@@ -175,11 +187,7 @@ export const SelectStockOwnerForm = ({
                     </Select>
                 </StyledFormItem>
                 <CameraScanner camData={{ setCamData }} handleCleanData={handleCleanData} />
-                <RadioButtons
-                    input={{ ...buttons }}
-                    output={{ onBack }}
-                    action1Label={t('actions:back')}
-                ></RadioButtons>
+                <RadioButtons input={{ ...buttons }} output={{ onBack }}></RadioButtons>
             </StyledForm>
         </WrapperForm>
     );
