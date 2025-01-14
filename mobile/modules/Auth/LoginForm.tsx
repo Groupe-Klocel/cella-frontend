701cf18aa6b36791513fc3b0c3cb54c0c77f7868
klocel-auto <null>
2025-01-14 09:52:25 +0000

    🔄 synced local 'mobile/' with remote 'mobile/'

--- mobile/modules/Auth/LoginForm.tsx
+++ mobile/modules/Auth/LoginForm.tsx
@@ -25,76 +25,73 @@ import { useAuth } from 'context/AuthContext';
 import useTranslation from 'next-translate/useTranslation';
 import { FC } from 'react';
 
-export interface ILoginFormProps {
+export interface ILoginFormProps {}
 
-}
+export const LoginForm: FC<ILoginFormProps> = ({}: ILoginFormProps) => {
+    const { t } = useTranslation('common');
+    const { login } = useAuth();
+    // TEXTS TRANSLATION
 
-export const LoginForm: FC<ILoginFormProps> = ({ }: ILoginFormProps) => {
-	const { t } = useTranslation('common')
-	const { login } = useAuth()
-	// TEXTS TRANSLATION
+    const welcome = t('welcome');
+    const username = t('username');
+    const password = t('password');
+    const workspace = t('workspace');
+    const forgotPassword = t('forgot-password');
+    const loginButton = t('actions:login');
+    const errorMessageUsername = t('messages:error-message-empty-input');
+    const errorMessagePassword = t('messages:error-message-empty-input');
 
-	const welcome = t('welcome')
-	const username = t('username')
-	const password = t('password')
-	const workspace = t('workspace')
-	const forgotPassword = t('forgot-password')
-	const loginButton = t('actions:login')
-	const errorMessageUsername = t('messages:error-message-empty-input')
-	const errorMessagePassword = t('messages:error-message-empty-input')
+    // END TEXTS TRANSLATION
 
-	// END TEXTS TRANSLATION
+    const [form] = Form.useForm();
 
-	const [form] = Form.useForm();
+    const onFinish = (values: any) => {
+        login({ username: values.username, password: values.password });
+    };
 
-	const onFinish = (values: any) => {
-		login({ username: values.username, password: values.password })
-	};
-
-
-	return (
-		<WrapperLogin>
-			<Logo title={META_DEFAULTS.title} />
-			<WelcomeText>
-				{welcome}
-			</WelcomeText>
-			<StyledForm
-				form={form}
-				name="login"
-				onFinish={onFinish}
-				autoComplete="off"
-				scrollToFirstError
-			>
-				<Form.Item
-					name="username"
-					rules={[{ required: true, message: errorMessageUsername }]}
-				>
-					<Input
-						prefix={<UserOutlined />}
-						style={{ height: '50px', fontSize: '16px' }}
-						placeholder={username} />
-				</Form.Item>
-				<Form.Item
-					name="password"
-					rules={[{ required: true, message: errorMessagePassword }]}
-				>
-					<Input
-						prefix={<LockOutlined />}
-						type="password"
-						placeholder={password}
-						style={{ height: '50px', fontSize: '16px' }} />
-				</Form.Item>
-				<Form.Item>
-					<Button
-						type="primary"
-						htmlType="submit"
-						block
-						style={{ height: '50px', fontSize: '16px' }}
-					>
-						{loginButton}
-					</Button>
-				</Form.Item>
-			</StyledForm>
-		</WrapperLogin>
-	);
-}
+    return (
+        <WrapperLogin>
+            <Logo title={META_DEFAULTS.title} />
+            <WelcomeText>{welcome}</WelcomeText>
+            <StyledForm
+                form={form}
+                name="login"
+                onFinish={onFinish}
+                autoComplete="off"
+                scrollToFirstError
+            >
+                <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: errorMessageUsername }]}
+                >
+                    <Input
+                        prefix={<UserOutlined />}
+                        style={{ height: '50px', fontSize: '16px' }}
+                        placeholder={username}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: errorMessagePassword }]}
+                >
+                    <Input
+                        prefix={<LockOutlined />}
+                        type="password"
+                        placeholder={password}
+                        style={{ height: '50px', fontSize: '16px' }}
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        block
+                        style={{ height: '50px', fontSize: '16px' }}
+                    >
+                        {loginButton}
+                    </Button>
+                </Form.Item>
+            </StyledForm>
+        </WrapperLogin>
+    );
+};
