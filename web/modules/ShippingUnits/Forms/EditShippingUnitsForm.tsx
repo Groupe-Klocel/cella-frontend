88af4fd7b91e1b2c6b2a72831f725b8831c459d0
klocel-auto <null>
2025-02-17 15:48:36 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/modules/ShippingUnits/Forms/EditShippingUnitsForm.tsx
+++ web/modules/ShippingUnits/Forms/EditShippingUnitsForm.tsx
@@ -63,6 +63,7 @@ export const EditShippingUnitsForm: FC<EditShippingUnitsFormProps> = ({
                         id
                         status
                         name
+                        weight
                     }
                 }
             }
