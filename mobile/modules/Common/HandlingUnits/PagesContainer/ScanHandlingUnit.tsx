7ec49f4b60cb246f996a4465016cb44bdf71420d
klocel-auto <null>
2025-04-22 13:12:17 +0000

    🔄 synced local 'mobile/' with remote 'mobile/'

--- mobile/modules/Common/HandlingUnits/PagesContainer/ScanHandlingUnit.tsx
+++ mobile/modules/Common/HandlingUnits/PagesContainer/ScanHandlingUnit.tsx
@@ -252,6 +252,7 @@ export const ScanHandlingUnit = ({
         scannedInfo: { scannedInfo, setScannedInfo },
         handlingUnitInfos,
         trigger: { triggerRender, setTriggerRender },
+        isEnforcedValue: !!enforcedValue,
         setResetForm
     };
 
