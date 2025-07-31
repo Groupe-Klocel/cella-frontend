b086116f2b3029c6aef10b4b488647c2f4a38c41
klocel-auto <null>
2025-07-31 12:57:41 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/modules/Locations/Forms/AddLocationForm.tsx
+++ web/modules/Locations/Forms/AddLocationForm.tsx
@@ -164,6 +164,7 @@ export const AddLocationForm = () => {
     }, [unsavedChanges]);
 
     const bulkCreateLocation = async (formData: any) => {
+        const blockId = formData['input']['blockId'];
         setCreateLoading(true);
         try {
             const query = gql`
@@ -183,16 +184,22 @@ export const AddLocationForm = () => {
 
             const response = await graphqlRequestClient.request(query, variables);
 
-            if (response?.executeFunction?.status === 'OK') {
-                showSuccess(t('messages:success-created'));
-                router.push(`/locations`);
+            if (response.executeFunction.status === 'ERROR') {
+                showError(response.executeFunction.output);
+            } else if (
+                response.executeFunction.status === 'OK' &&
+                response.executeFunction.output.status === 'KO'
+            ) {
+                showError(t(`errors:${response.executeFunction.output.output.code}`));
+                console.log('Backend_message', response.executeFunction.output.output);
             } else {
-                showError(t('messages:error-creating-data'));
+                showSuccess(t('messages:success-created'));
+                router.push(`/blocks/${blockId}`);
             }
+            setCreateLoading(false);
         } catch (error) {
-            console.error(error);
-            showError(t('messages:error-creating-data'));
-        } finally {
+            showError(t('messages:error-executing-function'));
+            console.log('executeFunctionError', error);
             setCreateLoading(false);
         }
     };
