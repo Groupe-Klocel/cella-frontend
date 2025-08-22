e0ba21cfd13840ed78f70a3a790c50e88f61cde5
klocel-auto <null>
2025-08-22 15:24:48 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/pages/api/handling-unit-contents/index.ts
+++ web/pages/api/handling-unit-contents/index.ts
@@ -138,6 +138,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                     quantity
                     stockStatus
                     reservation
+                    lastTransactionId
                 }
             }
         `;
