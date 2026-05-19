85d1a69d322541baf5b096e377316e98027a7567
klocel-auto <null>
2026-05-19 10:22:07 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/modules/Locations/Elements/LocationDetailsExtra.tsx
+++ web/modules/Locations/Elements/LocationDetailsExtra.tsx
@@ -17,16 +17,59 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 **/
-import { Typography } from 'antd';
-
-const { Title } = Typography;
+import { getModesFromPermissions } from '@helpers';
+import { useAppState } from 'context/AppContext';
+import { ModeEnum, Table } from 'generated/graphql';
+import { LocationExtrasListComponent } from './LocationExtrasListComponent';
+import { useMemo } from 'react';
 
 export interface IItemDetailsProps {
     locationId?: string | any;
+    locationName?: string | any;
+    locationCategory?: number | any;
+    details?: any;
 }
 
-const LocationDetailsExtra = ({ locationId }: IItemDetailsProps) => {
-    return <></>;
+const LocationDetailsExtra = ({
+    locationId,
+    locationName,
+    locationCategory
+}: IItemDetailsProps) => {
+    const { permissions, configs } = useAppState();
+    const modes = getModesFromPermissions(permissions, Table.Location);
+
+    const configsParamsCodes = useMemo(() => {
+        const findCodeByScopeAndValue = (items: any[], scope: string, value: string) => {
+            return items.find(
+                (item: any) =>
+                    item.scope === scope && item.value.toLowerCase() === value.toLowerCase()
+            )?.code;
+        };
+        return {
+            locationCategoryDockCode: findCodeByScopeAndValue(
+                configs ?? [],
+                'location_category',
+                'Dock'
+            )
+        };
+    }, [configs]);
+
+    return (
+        <>
+            {modes.length > 0 &&
+            modes.includes(ModeEnum.Read) &&
+            locationCategory == parseInt(configsParamsCodes.locationCategoryDockCode) ? (
+                <>
+                    <LocationExtrasListComponent
+                        locationId={locationId}
+                        locationName={locationName}
+                    />
+                </>
+            ) : (
+                <></>
+            )}
+        </>
+    );
 };
 
 export { LocationDetailsExtra };
