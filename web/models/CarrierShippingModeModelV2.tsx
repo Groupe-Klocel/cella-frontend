5dbeea9e62acf5f04f55a21e107c26a1a373fe62
klocel-auto <null>
2026-06-02 07:53:05 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/models/CarrierShippingModeModelV2.tsx
+++ web/models/CarrierShippingModeModelV2.tsx
@@ -85,7 +85,7 @@ export const CarrierShippingModeModelV2: ModelType = {
             isDefaultHiddenList: false,
             isExcludedFromList: false,
             isSortable: true,
-            searchingFormat: 'String',
+            searchingFormat: null,
             isDetailRequested: true,
             isExcludedFromDetail: false,
             detailGroup: '{"label":"general", "position":0}',
