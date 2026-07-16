d798eb3317d9f79512412c621916ff91bd26d30b
klocel-auto <null>
2026-07-16 12:34:06 +0000

    🔄 synced local 'web/' with remote 'web/'

---
+++ web/pages/custom-permissions/add/index.tsx
@@ -0,0 +1,56 @@
+/**
+CELLA Frontend
+Website and Mobile templates that can be used to communicate
+with CELLA WMS APIs.
+Copyright (C) 2023 KLOCEL <contact@klocel.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+**/
+import { AppHead, HeaderContent } from '@components';
+import { useRouter } from 'next/router';
+import { FC } from 'react';
+import MainLayout from '../../../components/layouts/MainLayout';
+import { CustomPermissionModelV2 } from '@helpers';
+import { AddEditItemComponent } from 'modules/Crud/AddEditItemComponentV2';
+import { useTranslationWithFallback as useTranslation } from '@helpers';
+import { addCustomPermissionRoutes } from 'modules/CustomPermissions/Static/customPermissionsRoutes';
+
+type PageComponent = FC & { layout: typeof MainLayout };
+
+const AddCustomPermissionPage: PageComponent = () => {
+    const { t } = useTranslation();
+
+    const router = useRouter();
+
+    return (
+        <>
+            <AppHead title={t('actions:add-custom-permission')} />
+            <AddEditItemComponent
+                dataModel={CustomPermissionModelV2}
+                headerComponent={
+                    <HeaderContent
+                        title={t('actions:add-custom-permission')}
+                        routes={addCustomPermissionRoutes}
+                        onBack={() => router.push(`/custom-permissions`)}
+                    />
+                }
+                routeAfterSuccess={`/custom-permissions/:id`}
+            />
+        </>
+    );
+};
+
+AddCustomPermissionPage.layout = MainLayout;
+
+export default AddCustomPermissionPage;
