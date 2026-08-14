b89c04f976782003546a9f1a4e9d971eebff0eef
klocel-auto <null>
2026-08-14 09:03:15 +0000

    🔄 synced local 'web/' with remote 'web/'

--- web/models/DeliveryAddressModelV2.tsx
+++ web/models/DeliveryAddressModelV2.tsx
@@ -521,6 +521,26 @@ export const DeliveryAddressModelV2: ModelType = {
             config: null,
             param: null
         },
+        entityDeliveryPointNumber: {
+            isListRequested: true,
+            isDefaultHiddenList: true,
+            isExcludedFromList: false,
+            isSortable: true,
+            searchingFormat: 'String',
+            isDetailRequested: true,
+            isExcludedFromDetail: false,
+            detailGroup: null,
+            link: null,
+            addEditFormat: 'String',
+            addEditStep: null,
+            maxLength: null,
+            displayName: null,
+            isMandatory: false,
+            minRule: null,
+            maxRule: null,
+            config: null,
+            param: null
+        },
         reference1: {
             isListRequested: true,
             isDefaultHiddenList: false,
